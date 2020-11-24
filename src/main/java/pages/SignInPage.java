8f3db02d315782db0e5b6a96c3912fb08e4030c8
ilnur <ilnur.nasybullin2000@yandex.ru>
2020-11-24 11:33:33 +0300

    add implicit waiting

--- src/main/java/pages/SignInPage.java
+++ src/main/java/pages/SignInPage.java
@@ -3,10 +3,13 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class SignInPage implements HTMLPage {
 
     public static final String URL = "http://localhost:9001/email";
     public static final String TITLE = "Sign In";
+    public static final int MAX_TIME_OUT = 5;
 
     private WebDriver driver;
 
@@ -15,6 +18,7 @@ public class SignInPage implements HTMLPage {
     @Override
     public void init(WebDriver driver) {
         this.driver = driver;
+        this.driver.manage().timeouts().implicitlyWait(MAX_TIME_OUT, TimeUnit.SECONDS);
     }
 
     public void open() {
